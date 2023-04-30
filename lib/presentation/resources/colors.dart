6ca04b362c0fbfb0366681c80ee78896d85a1dcc
ayman attieh <aymanattieh77@gmail.com>
2023-04-30 21:20:27 +0300

    add app colors

---
+++ lib/presentation/resources/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color darkBlue = Color(0xff100B20);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xffD8D8D8);
+  static const Color yallow = Color(0xffFFDD4F);
+  static const Color darkGrey = Color(0xff979797);
+  static const Color orange = Color(0xffEF8262);
+  static const Color black = Color(0xff000000);
+}
