dcef05fca835267894954101f023c992c840fcea
ayman attieh <aymanattieh77@gmail.com>
2023-04-30 21:06:04 +0300

    init repo

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/presentation/splash/view/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
