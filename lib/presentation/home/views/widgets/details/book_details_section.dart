82353cbbe6379d716b4cb5cc2764fc2f57f4932c
ayman attieh <aymanattieh77@gmail.com>
2023-05-02 20:27:46 +0300

    create book details section

---
+++ lib/presentation/home/views/widgets/details/book_details_section.dart
@@ -0,0 +1,35 @@
+import 'package:bookly/presentation/home/views/widgets/common/book_rating.dart';
+import 'package:bookly/presentation/home/views/widgets/details/book_details_image.dart';
+import 'package:bookly/presentation/resources/colors.dart';
+import 'package:bookly/presentation/resources/styles.dart';
+import 'package:bookly/presentation/resources/values.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const BookDetailsImage(),
+        const SizedBox(height: AppSizes.s20),
+        const Text(
+          'The Jungle Book',
+          style: AppStyles.textStyle30,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: AppSizes.s10),
+        Text(
+          'Rudyard Kipling',
+          style: AppStyles.textStyle16.copyWith(
+              color: ColorManager.white.withOpacity(0.7),
+              fontStyle: FontStyle.italic),
+        ),
+        const SizedBox(height: AppSizes.s15),
+        const BookRating(),
+      ],
+    );
+  }
+}
--- lib/presentation/home/views/widgets/details/book_details_view_body.dart
+++ lib/presentation/home/views/widgets/details/book_details_view_body.dart
@@ -1,12 +1,8 @@
-import 'package:bookly/presentation/home/views/widgets/common/book_rating.dart';
-import 'package:bookly/presentation/home/views/widgets/details/book_details_image.dart';
+import 'package:bookly/presentation/home/views/widgets/details/book_details_section.dart';
 import 'package:bookly/presentation/home/views/widgets/details/book_price_preview_widget.dart';
 import 'package:bookly/presentation/home/views/widgets/details/custom_book_details_appbar.dart';
 import 'package:bookly/presentation/home/views/widgets/details/see_also_books_section.dart';
 
-import 'package:bookly/presentation/resources/colors.dart';
-import 'package:bookly/presentation/resources/strings.dart';
-import 'package:bookly/presentation/resources/styles.dart';
 import 'package:bookly/presentation/resources/values.dart';
 import 'package:flutter/material.dart';
 
@@ -16,30 +12,15 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        const CustomBookDetailsAppbar(),
-        const SizedBox(height: AppPadding.p10),
-        const BookDetailsImage(),
-        const SizedBox(height: AppSizes.s20),
-        const Text(
-          'The Jungle Book',
-          style: AppStyles.textStyle30,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: AppSizes.s10),
-        Text(
-          'Rudyard Kipling',
-          style: AppStyles.textStyle16.copyWith(
-              color: ColorManager.white.withOpacity(0.7),
-              fontStyle: FontStyle.italic),
-        ),
-        const SizedBox(height: AppSizes.s15),
-        const BookRating(),
-        const SizedBox(height: AppSizes.s30),
-        const BookPriceAndPreview(),
-        const SizedBox(height: AppSizes.s20),
-        const SeeAlsoBooksSection(),
-        const SizedBox(height: AppSizes.s15),
+      children: const [
+        CustomBookDetailsAppbar(),
+        SizedBox(height: AppSizes.s10),
+        BookDetailsSection(),
+        SizedBox(height: AppSizes.s30),
+        BookPriceAndPreview(),
+        SizedBox(height: AppSizes.s20),
+        SeeAlsoBooksSection(),
+        SizedBox(height: AppSizes.s15),
       ],
     );
   }
