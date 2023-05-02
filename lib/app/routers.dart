61cf90d4c791429ab0f57535956725ba8d641c8a
ayman attieh <aymanattieh77@gmail.com>
2023-05-02 17:07:27 +0300

    update app navigation

---
+++ lib/app/routers.dart
@@ -0,0 +1,32 @@
+// GoRouter configuration
+import 'package:bookly/presentation/home/views/book_details_view.dart';
+import 'package:bookly/presentation/home/views/home_view.dart';
+import 'package:bookly/presentation/splash/view/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class Routers {
+  static const String splash = '/';
+  static const String home = '/home';
+  static const String details = '/details';
+}
+
+abstract class AppRouter {
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: Routers.splash,
+        builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: Routers.home,
+        builder: (context, state) => const HomeView(),
+        routes: [
+          GoRoute(
+            path: Routers.details,
+            builder: (context, state) => const BookDetailsView(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
