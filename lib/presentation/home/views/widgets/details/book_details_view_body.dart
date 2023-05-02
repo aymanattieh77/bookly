46e72f8e003a7ce02efc5e3b5bf0ce476300aa8e
ayman attieh <aymanattieh77@gmail.com>
2023-05-02 21:27:58 +0300

    update book details view body

--- lib/presentation/home/views/widgets/details/book_details_view_body.dart
+++ lib/presentation/home/views/widgets/details/book_details_view_body.dart
@@ -11,16 +11,23 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        CustomBookDetailsAppbar(),
-        SizedBox(height: AppSizes.s10),
-        BookDetailsSection(),
-        SizedBox(height: AppSizes.s30),
-        BookPriceAndPreview(),
-        SizedBox(height: AppSizes.s20),
-        SeeAlsoBooksSection(),
-        SizedBox(height: AppSizes.s15),
+    return CustomScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      slivers: [
+        SliverFillRemaining(
+          child: Column(
+            children: const [
+              CustomBookDetailsAppbar(),
+              SizedBox(height: AppSizes.s10),
+              BookDetailsSection(),
+              SizedBox(height: AppSizes.s30),
+              BookPriceAndPreview(),
+              SizedBox(height: AppSizes.s20),
+              SeeAlsoBooksSection(),
+              SizedBox(height: AppSizes.s15),
+            ],
+          ),
+        ),
       ],
     );
   }
