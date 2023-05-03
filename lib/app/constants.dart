a3c479f0bd3c1e9cbb6c92300cab5064fa374529
ayman attieh <aymanattieh77@gmail.com>
2023-05-03 22:25:58 +0300

    create dio factory

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -3,4 +3,5 @@ const kSplashViewDuration = Duration(seconds: 3);
 
 abstract class AppConstants {
   static const baseUrl = 'https://www.googleapis.com/books/v1/';
+  static const timeoutDuration = 1000 * 60;
 }
