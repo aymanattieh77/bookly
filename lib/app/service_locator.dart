36369d22c41cb5aa44458a7be36aded57f34517e
ayman attieh <aymanattieh77@gmail.com>
2023-05-03 23:14:33 +0300

    update service locator

--- lib/app/service_locator.dart
+++ lib/app/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/data/date_source/remote_data_source.dart';
 import 'package:bookly/data/network/api_service.dart';
 import 'package:bookly/data/network/dio_factory.dart';
+import 'package:bookly/data/network/network_info.dart';
 import 'package:bookly/data/repository/repository_impl.dart';
 import 'package:bookly/domain/repository/repository.dart';
 import 'package:get_it/get_it.dart';
@@ -15,5 +16,7 @@ void setupAppService() {
 
   getIt.registerLazySingleton<RemoteDateSource>(
       () => RemoteDataSourceImpl(getIt()));
-  getIt.registerLazySingleton<Repository>(() => RepositoryImpl(getIt()));
+  getIt.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl());
+  getIt.registerLazySingleton<Repository>(
+      () => RepositoryImpl(getIt(), getIt()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:bookly/app/app.dart';
-import 'package:bookly/data/date_source/remote_data_source.dart';
-import 'package:bookly/data/network/api_service.dart';
-import 'package:bookly/data/repository/repository_impl.dart';
 import 'package:bookly/domain/repository/repository.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-
 import 'app/service_locator.dart';
 
 void main() async {
